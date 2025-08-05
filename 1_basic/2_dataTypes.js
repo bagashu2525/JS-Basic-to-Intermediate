0a6e3c3928d0ce4ddedefcc0481ad83dba16bee9
Asmita Bag <113798160+bagashu2525@users.noreply.github.com>
2025-08-05 15:33:34 +0000

    discussion of data types

---
+++ 1_basic/2_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+/**Primitive Datatype */
+// number => 2 ^ 53
+//bigint
+//String => ""
+//boolean => true/false
+// null => standalone value
+//undefined => the variable is empty
+// symbol (it will be in use when we will learn react for making any component unique)
+
+//object
+
+
+//type of undefined is a undefined but that of null is an object
