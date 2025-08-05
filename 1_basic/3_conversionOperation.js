7382bfac2ca76f027dfdcd2c99da50e5c466c81d
Asmita Bag <113798160+bagashu2525@users.noreply.github.com>
2025-08-05 15:34:12 +0000

    discussion of Conversions

---
+++ 1_basic/3_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33abc"
+console.log(typeof score);
+
+let valueIntNumber = Number(score);
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber); //NaN => Not a Number
+
+score="33"
+console.log(typeof score);
+
+valueIntNumber = Number(score);
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber); //33
+
+// Boolean() 
+// 1=> true; 0=> false
+//"hitesh" => true; ""=>false
+
+//String()
