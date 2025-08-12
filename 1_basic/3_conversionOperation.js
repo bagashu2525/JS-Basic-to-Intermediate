e17275f5e22fa64c56d5753b9bc48acc43e4636c
Asmita Bag <113798160+bagashu2525@users.noreply.github.com>
2025-08-12 05:38:34 +0000

    add trickey operations in js

--- 1_basic/3_conversionOperation.js
+++ 1_basic/3_conversionOperation.js
@@ -17,3 +17,29 @@ console.log(valueIntNumber); //33
 //"hitesh" => true; ""=>false
 
 //String()
+
+
+// *************************** Operations  *************************
+
+let value = 3
+let negValue = -value
+
+console.log(negValue) //negative value
+
+//string concatenation 
+let str1="Asmita"
+let str2="Bag"
+let str3=str1+str2
+
+console.log(str3)
+
+//something new or trickey conversion
+console.log("1"+2);
+console.log(1+"2");
+
+console.log("1"+2+2); //string is in first all conversion is in string
+console.log(1+2+"2"); //number is in first numbers are added first and then string in concate
+
+console.log(+true); //1
+console.log(+"")//0
+
